91bc3e317b3cf83315f3a855b08d8332e7d069a0
Hoon Wee <mrgravity817@gmail.com>
2022-05-02 19:10:48 +0900

    Make app to run in background in test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,23 +1,26 @@
 #[tokio::test]
 async fn health_check_works() {
-	// Arrange
-	spawn_app().await.expect("Failed to spawn our app");
-	// We need to bring in `reqwest`
-	// to perform HTTP request against our application
-	let client = reqwest::Client::new();
+    // Arrange
+    spawn_app();
+    // We need to bring in `reqwest`
+    // to perform HTTP request against our application
+    let client = reqwest::Client::new();
 
-	// Act
-	let response = client
-				.get("http://127.0.0.1:8000/health_check")
-				.send()
-				.await
-				.expect("Failed to execute request.");
-	
-	// Assert
-	assert!(response.status().is_success());
-	assert_eq!(Some(0), response.content_length());
+    // Act
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
 }
 
-async fn spawn_app() -> std::io::Result<()> {
-	z2p::run().await
+fn spawn_app() {
+    let server = z2p::run().expect("Failed to bind address");
+    // Lauch the server as a background task
+    // tokio::spawn returns a handle to the spawned future
+    let _ = tokio::spawn(server);
 }
