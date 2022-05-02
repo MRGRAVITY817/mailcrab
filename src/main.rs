f0f7b325ea248dc81f861a987e565547f3ef5606
Hoon Wee <mrgravity817@gmail.com>
2022-05-02 19:26:55 +0900

    Server uses TcpListener to handle multiple ports in future

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::net::TcpListener;
 use z2p::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0")?;
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,14 +1,14 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
-    // We need to bring in `reqwest`
-    // to perform HTTP request against our application
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -18,9 +18,11 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = z2p::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = z2p::run(listener).expect("Failed to bind address");
     // Lauch the server as a background task
-    // tokio::spawn returns a handle to the spawned future
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
