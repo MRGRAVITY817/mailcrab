70acef06a2d38ff119bce05b7f636060386ea602
Hoon Wee <mrgravity817@gmail.com>
2022-05-19 00:49:41 +0900

    Add tracing subscriber that outputs JSON logs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
-use env_logger::Env;
-
 use {
     mailcrab::{configuration::get_configuration, startup::run},
     sqlx::PgPool,
     std::net::TcpListener,
+    tracing::subscriber::set_global_default,
+    tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer},
+    tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry},
 };
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // set logger
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // This will filter the RUST_LOG env variable. Defaulted as `RUST_LOG=info`.
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    // This will format log from JSON to Bunyan. https://github.com/trentm/node-bunyan
+    let formatting_layer = BunyanFormattingLayer::new("mailcrab".into(), std::io::stdout);
+    // Subscriber will trace the spans, with awesome JSON formatted logs
+    // which can be ingested easily by Elastic Search.
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subscriber).expect("Failed to set subscriber");
+
+    // Configuration
     let app_config = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&app_config.database.connection_string())
         .await
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -6,6 +6,7 @@ use {
     chrono::Utc,
     serde::Deserialize,
     sqlx::PgPool,
+    tracing::Instrument,
     uuid::Uuid,
 };
 
@@ -24,12 +25,9 @@ pub async fn subscribe(form: Form<FormData>, pool: Data<PgPool>) -> HttpResponse
         subscriber_email = %form.email,
         subscriber_name = %form.name
     );
-
     let _request_span_guard = request_span.enter();
-    tracing::info!(
-        "request_id {} - Saving new subscriber details in the database.",
-        request_id,
-    );
+
+    let query_span = tracing::info_span!("Saving new subscriber details in the database.");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -41,21 +39,12 @@ pub async fn subscribe(form: Form<FormData>, pool: Data<PgPool>) -> HttpResponse
         Utc::now()
     )
     .execute(pool.get_ref())
+    .instrument(query_span) // Instrument will track future's polling lifetime
     .await
     {
-        Ok(_) => {
-            tracing::info!(
-                "request_id {} - New subscriber details have been saved.",
-                request_id
-            );
-            HttpResponse::Ok().finish()
-        }
+        Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
-            tracing::error!(
-                "request_id {} - Failed to execute query: {:?}",
-                request_id,
-                e
-            );
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
