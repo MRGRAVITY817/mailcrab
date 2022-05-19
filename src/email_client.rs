38e102278744968c2cca8cb5f704acbc018fbffd
Hoon Wee <mrgravity817@gmail.com>
2022-05-20 02:10:37 +0900

    Better send email unit test

--- src/email_client.rs
+++ src/email_client.rs
@@ -50,6 +50,7 @@ impl EmailClient {
 }
 
 #[derive(Serialize)]
+#[serde(rename_all = "PascalCase")]
 struct SendEmailRequest {
     from: String,
     to: String,
@@ -71,17 +72,43 @@ mod tests {
             Fake, Faker,
         },
         secrecy::Secret,
-        wiremock::{matchers::any, Mock, MockServer, ResponseTemplate},
+        wiremock::{
+            matchers::{header, header_exists, method, path},
+            Mock, MockServer, Request, ResponseTemplate,
+        },
     };
 
+    struct SendEmailBodyMatcher;
+
+    impl wiremock::Match for SendEmailBodyMatcher {
+        fn matches(&self, request: &Request) -> bool {
+            let result: Result<serde_json::Value, _> = serde_json::from_slice(&request.body);
+
+            if let Ok(body) = result {
+                dbg!(&body);
+                body.get("From").is_some()
+                    && body.get("To").is_some()
+                    && body.get("Subject").is_some()
+                    && body.get("HtmlBody").is_some()
+                    && body.get("TextBody").is_some()
+            } else {
+                false
+            }
+        }
+    }
+
     #[tokio::test]
-    async fn send_email_fires_a_request_to_base_url() {
+    async fn send_email_sends_the_expected_request() {
         // Arrange
         let mock_server = MockServer::start().await;
         let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
         let email_client = EmailClient::new(mock_server.uri(), sender, Secret::new(Faker.fake()));
 
-        Mock::given(any())
+        Mock::given(header_exists("X-Postmark-Server-Token"))
+            .and(header("Content-Type", "application/json"))
+            .and(path("/email"))
+            .and(method("POST"))
+            .and(SendEmailBodyMatcher)
             .respond_with(ResponseTemplate::new(200))
             .expect(1) // We expect to fire only one http request
             .mount(&mock_server)
