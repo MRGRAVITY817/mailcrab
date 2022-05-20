0aebea207392489a152ca784ab4f67bafeebf88e
Hoon Wee <mrgravity817@gmail.com>
2022-05-20 19:45:51 +0900

    Set default confirmation status to pending

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -23,12 +23,32 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     // Assert
     assert_eq!(200, response.status().as_u16());
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+}
+
+#[tokio::test]
+async fn subscribe_persists_the_new_subscriber() {
+    // Arrange
+    let test_app = spawn_app().await;
+    let body = "name=hoon%20wee&email=mrgravity817%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&test_app.email_server)
+        .await;
+
+    // Act
+    test_app.post_subscriptions(body.into()).await;
+
+    // Assert
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions")
         .fetch_one(&test_app.db_pool)
         .await
         .expect("Failed to fetch saved subscriptions.");
+
     assert_eq!(saved.email, "mrgravity817@gmail.com");
     assert_eq!(saved.name, "hoon wee");
+    assert_eq!(saved.status, "pending_confirmation");
 }
 
 #[tokio::test]
