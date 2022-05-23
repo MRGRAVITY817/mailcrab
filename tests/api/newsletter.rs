d3caaf6d41820da791bbcd78728321c6216dc80b
Hoon Wee <mrgravity817@gmail.com>
2022-05-23 18:18:50 +0900

    Add test for `/email` endpoint which doesn't work yet

---
+++ tests/api/newsletter.rs
@@ -0,0 +1,53 @@
+use crate::helpers::{spawn_app, TestApp};
+use wiremock::{
+    matchers::{any, method, path},
+    Mock, ResponseTemplate,
+};
+
+#[tokio::test]
+async fn newsletter_are_not_delivered_to_unconfirmed_subscribers() {
+    // Arrange
+    let test_app = spawn_app().await;
+    create_unconfirmed_subscriber(&test_app).await;
+
+    Mock::given(any())
+        .respond_with(ResponseTemplate::new(200))
+        .expect(0)
+        .mount(&test_app.email_server)
+        .await;
+
+    // Act
+    let newsletter_request_body = serde_json::json!({
+        "title": "Newsletter title",
+        "content": {
+            "text": "Newsletter body as plain text",
+            "html": "<p>Newsletter body as HTML</p>",
+        }
+    });
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &test_app.address))
+        .json(&newsletter_request_body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+async fn create_unconfirmed_subscriber(test_app: &TestApp) {
+    let body = "name=hoon%2wee&email=mrgravity817%40gmail.com";
+    let _mock_guard = Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .named("Create unconfirmed subscriber")
+        .expect(1)
+        .mount_as_scoped(&test_app.email_server)
+        .await;
+
+    test_app
+        .post_subscriptions(body.into())
+        .await
+        .error_for_status()
+        .unwrap();
+}
