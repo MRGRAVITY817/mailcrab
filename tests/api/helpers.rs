45575b5dbd1f0b13f6e0d09f6b983e63005979f0
Hoon Wee <mrgravity817@gmail.com>
2022-05-31 00:11:23 +0900

    Refactor redirection assertions using helper function

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -199,3 +199,9 @@ async fn configure_database(config: &DatabaseSettings) -> PgPool {
 
     db_pool
 }
+
+/// Commonly used assert test for redirection
+pub fn assert_is_redirect_to(response: &reqwest::Response, location: &str) {
+    assert_eq!(response.status().as_u16(), 303);
+    assert_eq!(response.headers().get("Location").unwrap(), location);
+}
