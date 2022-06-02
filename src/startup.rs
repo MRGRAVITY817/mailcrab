88422c25bdb677b5ecbb2856070184a532e2404a
Hoon Wee <mrgravity817@gmail.com>
2022-06-02 10:08:16 +0900

    Add change password get/post handler for admin

--- src/startup.rs
+++ src/startup.rs
@@ -3,8 +3,8 @@ use {
         configuration::{DatabaseSettings, Settings},
         email_client::EmailClient,
         routes::{
-            admin_dashboard, confirm, health_check, home, login_form, login_submit,
-            publish_newsletter, subscribe,
+            admin_dashboard, change_password, change_password_form, confirm, health_check, home,
+            login_form, login_submit, publish_newsletter, subscribe,
         },
     },
     actix_session::{storage::RedisSessionStore, SessionMiddleware},
@@ -105,6 +105,8 @@ async fn run(
             .route("/subscriptions/confirm", web::get().to(confirm))
             .route("/newsletters", web::post().to(publish_newsletter))
             .route("/admin/dashboard", web::get().to(admin_dashboard))
+            .route("/admin/password", web::get().to(change_password_form))
+            .route("/admin/password", web::post().to(change_password))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
