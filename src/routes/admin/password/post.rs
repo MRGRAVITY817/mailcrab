6869a8fcd8d21ce6ed78b91829a1b24fa0d1c60d
Hoon Wee <mrgravity817@gmail.com>
2022-06-02 12:13:10 +0900

    Add test: user should provide valid current password when changing one's password

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -1,5 +1,7 @@
 use {
     crate::{
+        authentication::{validate_credentials, AuthError, Credentials},
+        routes::admin::dashboard::get_username,
         session_state::TypedSession,
         utils::{e500, see_other},
     },
@@ -7,6 +9,7 @@ use {
     actix_web_flash_messages::FlashMessage,
     secrecy::ExposeSecret,
     secrecy::Secret,
+    sqlx::PgPool,
 };
 
 #[derive(serde::Deserialize)]
@@ -19,11 +22,18 @@ pub struct FormData {
 pub async fn change_password(
     form: web::Form<FormData>,
     session: TypedSession,
+    pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, actix_web::Error> {
     // Check if login session is active
     if session.get_user_id().map_err(e500)?.is_none() {
         return Ok(see_other("/login"));
     }
+    let user_id = session.get_user_id().map_err(e500)?;
+    // If given `user_id` doesn't exists in redis session, redirect to `login` page
+    if user_id.is_none() {
+        return Ok(see_other("/login"));
+    }
+    let user_id = user_id.unwrap();
     // Check if two input data for new password is equal
     if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
         // If not, send flash message to indicate error to user on page.
@@ -32,6 +42,23 @@ pub async fn change_password(
         )
         .send();
         return Ok(see_other("/admin/password"));
+    }
+
+    let username = get_username(user_id, &pool).await.map_err(e500)?;
+    let credentials = Credentials {
+        username,
+        password: form.0.current_password,
+    };
+
+    // Check if current password is valid
+    if let Err(e) = validate_credentials(credentials, &pool).await {
+        return match e {
+            AuthError::InvalidCredentials(_) => {
+                FlashMessage::error("The current password is incorrect.").send();
+                Ok(see_other("/admin/password"))
+            }
+            AuthError::UnexpectedError(_) => Err(e500(e)),
+        };
     }
     todo!()
 }
--- tests/api/change_password.rs
+++ tests/api/change_password.rs
@@ -1,6 +1,7 @@
-use uuid::Uuid;
-
-use crate::helpers::{assert_is_redirect_to, spawn_app};
+use {
+    crate::helpers::{assert_is_redirect_to, spawn_app},
+    uuid::Uuid,
+};
 
 #[tokio::test]
 async fn you_must_be_logged_in_to_see_the_change_password_form() {
@@ -64,8 +65,39 @@ async fn new_password_fields_must_match() {
     assert_is_redirect_to(&response, "/admin/password");
     // 2. Follow the redirect, check the html page
     let html_page = test_app.get_change_password_html().await;
-    dbg!(&html_page);
     assert!(html_page.contains(
         "<p><i>You entered two different new passwords - the field values must match.</i></p>"
-    ))
+    ));
+}
+
+#[tokio::test]
+async fn current_password_must_be_valid() {
+    // Arrange
+    let test_app = spawn_app().await;
+    let new_password = Uuid::new_v4().to_string();
+    let wrong_current_password = Uuid::new_v4().to_string();
+
+    // Act
+    // 1. Login
+    test_app
+        .post_login(&serde_json::json!({
+            "username": &test_app.test_user.username,
+            "password": &test_app.test_user.password,
+        }))
+        .await;
+    // 2. Try to change the password
+    let response = test_app
+        .post_change_password(&serde_json::json!({
+            "current_password": &wrong_current_password,
+            "new_password": &new_password,
+            "new_password_check": &new_password,
+        }))
+        .await;
+
+    // Assert
+    // 1. If current password is invalid, server will redirect user to `admin/password` page
+    assert_is_redirect_to(&response, "/admin/password");
+    // 2. Follow the redirect, check the html page
+    let html_page = test_app.get_change_password_html().await;
+    assert!(html_page.contains("<p><i>The current password is incorrect.</i></p>"));
 }
