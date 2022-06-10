274149dcef39a2f6a43b2f6d5a06ff3c16abe369
Hoon Wee <mrgravity817@gmail.com>
2022-06-10 23:35:16 +0900

    Add function that saves response

--- src/idempotency/persistence.rs
+++ src/idempotency/persistence.rs
@@ -1,6 +1,6 @@
 use super::IdempotencyKey;
-use actix_web::{http::StatusCode, HttpResponse};
-use sqlx::PgPool;
+use actix_web::{body::to_bytes, http::StatusCode, HttpResponse};
+use sqlx::{postgres::PgHasArrayType, PgPool};
 use uuid::Uuid;
 
 #[derive(Debug, sqlx::Type)]
@@ -42,5 +42,58 @@ pub async fn get_saved_response(
         Ok(Some(response.body(r.response_body)))
     } else {
         Ok(None)
+    }
+}
+
+/// Save HttpResponse to idempotency table
+pub async fn save_response(
+    pool: &PgPool,
+    idempotency_key: &IdempotencyKey,
+    user_id: Uuid,
+    http_response: HttpResponse,
+) -> Result<HttpResponse, anyhow::Error> {
+    let (response_head, body) = http_response.into_parts();
+    // Since `MessageBody::Error` is not `Send + Sync` type,
+    // we need to manually map the error
+    let body = to_bytes(body).await.map_err(|e| anyhow::anyhow!("{}", e))?;
+    let status_code = http_response.status().as_u16() as i16;
+    let headers = http_response
+        .headers()
+        .iter()
+        .map(|(name, value)| {
+            let name = name.as_str().to_owned();
+            let value = value.as_bytes().to_owned();
+            HeaderPairRecord { name, value }
+        })
+        .collect::<Vec<_>>();
+
+    sqlx::query_unchecked!(
+        r#"
+        INSERT INTO idempotency (
+            user_id,
+            idempotency_key,
+            response_status_code,
+            response_headers,
+            response_body,
+            created_at
+        )
+        VALUES ($1, $2, $3, $4, $5, now())
+        "#,
+        user_id,
+        idempotency_key.as_ref(),
+        status_code,
+        headers,
+        body.as_ref()
+    )
+    .execute(pool)
+    .await?;
+
+    let http_response = response_head.set_body(body).map_into_boxed_body();
+    Ok(http_response)
+}
+
+impl PgHasArrayType for HeaderPairRecord {
+    fn array_type_info() -> sqlx::postgres::PgTypeInfo {
+        sqlx::postgres::PgTypeInfo::with_name("_header_pair")
     }
 }
