e5efa70f9102783b4ab05d6426c9a23fdcf5cb1d
Hoon Wee <mrgravity817@gmail.com>
2022-06-15 22:30:04 +0900

    Add tracing to future selection

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use mailcrab::{
-    configuration::get_config,
-    issue_delivery_worker::run_worker_until_stopped,
-    startup::Application,
-    telemetry::{get_subscriber, init_subscriber},
+use {
+    mailcrab::{
+        configuration::get_config,
+        issue_delivery_worker::run_worker_until_stopped,
+        startup::Application,
+        telemetry::{get_subscriber, init_subscriber},
+    },
+    std::fmt::{Debug, Display},
+    tokio::task::JoinError,
 };
 
 #[tokio::main]
@@ -20,9 +24,24 @@ async fn main() -> anyhow::Result<()> {
     // Run both tasks concurrently / in parallel
     // this will run until one of the two tasks completes or errors out
     tokio::select! {
-        _ = app_task => {},
-        _ = worker_task => {},
+        api = app_task => report_exit("API", api),
+        background = worker_task => report_exit("Background worker", background),
     };
 
     Ok(())
+}
+
+/// Report how the future handler's been selected and exited
+fn report_exit(task_name: &str, outcome: Result<Result<(), impl Debug + Display>, JoinError>) {
+    match outcome {
+        Ok(Ok(())) => {
+            tracing::info!("{} has exited", task_name)
+        }
+        Ok(Err(e)) => {
+            tracing::error!(error.cause_chain = ?e, error.message = %e, "{} failed", task_name)
+        }
+        Err(e) => {
+            tracing::error!(error.cause_chain = ?e, error.message = %e, "{} task failed to complete", task_name)
+        }
+    }
 }
