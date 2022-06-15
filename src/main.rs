f3fc2a9bc6b1b94b0a605e5b367b8286cb1f6901
Hoon Wee <mrgravity817@gmail.com>
2022-06-15 21:53:09 +0900

    Main function should run publish worker and app concurrently

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use mailcrab::{
     configuration::get_config,
+    issue_delivery_worker::run_worker_until_stopped,
     startup::Application,
     telemetry::{get_subscriber, init_subscriber},
 };
@@ -10,8 +11,18 @@ async fn main() -> anyhow::Result<()> {
     init_subscriber(subscriber);
 
     let app_config = get_config().expect("Failed to read configuration");
-    let main_app = Application::build(app_config).await?;
-    main_app.run_until_stopped().await?;
+    let main_app = Application::build(app_config.clone()).await?;
+
+    // Create tasks to be in separate threads
+    let app_task = tokio::spawn(main_app.run_until_stopped());
+    let worker_task = tokio::spawn(run_worker_until_stopped(app_config));
+
+    // Run both tasks concurrently / in parallel
+    // this will run until one of the two tasks completes or errors out
+    tokio::select! {
+        _ = app_task => {},
+        _ = worker_task => {},
+    };
 
     Ok(())
 }
